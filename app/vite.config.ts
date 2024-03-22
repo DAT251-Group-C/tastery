80faedafd42faf441cf3a5369de844c352ac3edc
Eirik Måseidvåg <eirik.maaseidvaag@gmail.com>
2024-03-22 17:13:15 +0100

    sort package json, update engine requirements

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,4 +6,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   publicDir: path.resolve(__dirname, './public'),
   plugins: [vue(), tsconfigPaths()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    setupFiles: ['./unit.setup.ts'],
+  }
 });
